73377358fc61254814f0f47ed38ae949c64fca67
ali.sh <narenje20142014@gmail.com>
2021-08-16 03:39:26 +0430

    start project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// import components
+import "./component/Navbar";
+
+import "./App.scss";
+import Navbar from "./component/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
