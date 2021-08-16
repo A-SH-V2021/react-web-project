62d31b1fc9ad43a54f6efbf22615ba5202b7af52
ali.sh <narenje20142014@gmail.com>
2021-08-17 01:12:38 +0430

    add items

--- src/component/tour/Tour.js
+++ src/component/tour/Tour.js
@@ -1,5 +1,6 @@
 import React from "react";
-import icon from  '../../delete.png';
+import icon from "../../delete.png";
+import iconDown from "../../img/down-arrow.png";
 import "./Tour.scss";
 
 const Tour = () => {
@@ -10,8 +11,18 @@ const Tour = () => {
           src="https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
           alt="laptop"
         />
-        <img src={icon} alt="deleteIcon" />
-        
+        <img src={icon} alt="deleteIcon" className="delete-icon" />
+      </div>
+      <div className="tour-info">
+        <h3>picture</h3>
+        <h4>name</h4>
+        <h5>info
+          <img src={iconDown} alt="iconDown" className='iconDown'/>
+        </h5>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos,
+          consequuntur.
+        </p>
       </div>
     </article>
   );
