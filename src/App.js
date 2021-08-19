e7c4a087e0d7af7b5099e36e359f6ee66fca94e0
ali.sh <narenje20142014@gmail.com>
2021-08-19 22:26:18 +0430

    add components for rout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import components
 import Navbar from "./component/Navbar";
 import Tourlist from "./component/tourlist/Tourlist";
+import Home from  './component/Home'
+import About from  './component/About'
+import Error from  './component/Error'
 
 import "./App.scss";
 const App = () => {
   return (
-    <main>
-      <Navbar />
-      <Tourlist />
-    </main>
+    <Router>
+      <main>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/tours' component={Tourlist}/>
+          <Route path='/about' component={About}/>
+          <Route path='*' component={Error}/>
+        </Switch>
+      </main>
+    </Router>
   );
 };
 
---
+++ src/component/Error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className='error'>
+      <h2>Opssss... this page doesn't exist!!!</h2>
+    </div>
+  );
+};
+
+export default Error;
